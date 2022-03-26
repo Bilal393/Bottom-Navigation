b40364245234e7d1b98491962a397864c160be88
bilal <bilal.9hassan@gmail.com>
2022-03-26 13:03:29 +0500

    Bottom Navigation with Activity

---
+++ app/src/main/java/combilal/bottomNavigation/Info.java
@@ -0,0 +1,45 @@
+package combilal.bottomNavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.Info);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.Home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.Headset:
+                        startActivity(new Intent(getApplicationContext(), Headset.class));
+                        finish();
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.Info:
+                        startActivity(new Intent(getApplicationContext(), Info.class));
+                        finish();
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
